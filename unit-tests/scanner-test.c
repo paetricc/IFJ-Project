1144ea3a9632e20312c976c110fecf8be53f6b28
Šimon Vacek <simonvacky@email.cz>
2021-11-09 00:21:58 +0100

    feat: pridana funkce Token_ID_to_string
    Funkce vypise string odpovidajici enum hodnote ID.
    fix: taky jsem upravil spatne zapsany default case predchozi funkce

--- unit-tests/scanner-test.c
+++ unit-tests/scanner-test.c
@@ -41,8 +41,81 @@ char *keyword_to_string(Keyword keyword) {
             return "KEYWORD_THEN";
         case KEYWORD_WHILE:
             return "KEYWORD_WHILE";
-        case default:
+        default:
             return NULL;
+    }
+}
+
+/**
+ * Prelozi enum hodnotu Token_ID na string obsahujici jeho nazev.
+ *
+ * @param keyword Hodnota udavajici o jake ID tokenu se jedna
+ * @return String literal odpovidajici id tokenu
+*/
+char *Token_ID_to_string(Token_ID id) {
+    switch(id) {
+        case TOKEN_ID_KEYWORD:
+            return "TOKEN_ID_KEYWORD";
+        case TOKEN_ID_EOL:
+            return "TOKEN_ID_EOL";
+        case TOKEN_ID_CMA:
+            return "TOKEN_ID_CMA";
+        case TOKEN_ID_SPACE:
+            return "TOKEN_ID_SPACE";
+        case TOKEN_ID_RBR:
+            return "TOKEN_ID_RBR";
+        case TOKEN_ID_LBR:
+            return "TOKEN_ID_LBR";
+        case TOKEN_ID_ADD:
+            return "TOKEN_ID_ADD";
+        case TOKEN_ID_MUL:
+            return "TOKEN_ID_MUL";
+        case TOKEN_ID_SUB:
+            return "TOKEN_ID_SUB";
+        case TOKEN_ID_LCMT2:
+            return "TOKEN_ID_LCMT2";
+        case TOKEN_ID_BCMT4:
+            return "TOKEN_ID_BCMT4";
+        case TOKEN_ID_DIV2:
+            return "TOKEN_ID_DIV2";
+        case TOKEN_ID_DIV:
+            return "TOKEN_ID_DIV";
+        case TOKEN_ID_NEQ2:
+            return "TOKEN_ID_NEQ2";
+        case TOKEN_ID_LTE:
+            return "TOKEN_ID_LTE";
+        case TOKEN_ID_LT:
+            return "TOKEN_ID_LT";
+        case TOKEN_ID_GTE:
+            return "TOKEN_ID_GTE";
+        case TOKEN_ID_GT:
+            return "TOKEN_ID_GT";
+        case TOKEN_ID_EQ:
+            return "TOKEN_ID_EQ";
+        case TOKEN_ID_ASSIGN:
+            return "TOKEN_ID_ASSIGN";
+        case TOKEN_ID_FSTR:
+            return "TOKEN_ID_FSTR";
+        case TOKEN_ID_STR5:
+            return "TOKEN_ID_STR5";
+        case TOKEN_ID_INT0:
+            return "TOKEN_ID_INT0";
+        case TOKEN_ID_HEX2:
+            return "TOKEN_ID_HEX2";
+        case TOKEN_ID_DHEX2:
+            return "TOKEN_ID_DHEX2";
+        case TOKEN_ID_HEXP3:
+            return "TOKEN_ID_HEXP3";
+        case TOKEN_ID_EXP3:
+            return "TOKEN_ID_EXP3";
+        case TOKEN_ID_DBL2:
+            return "TOKEN_ID_DBL2";
+        case TOKEN_ID_INT:
+            return "TOKEN_ID_INT";
+        case TOKEN_ID_ID:
+            return "TOKEN_ID_ID";
+        case TOKEN_ID_EOF:
+            return "TOKEN_ID_EOF";
     }
 }
 
@@ -52,7 +125,9 @@ char *keyword_to_string(Keyword keyword) {
  * @param *token Ukazatel na strukturu tokenu
 */
 void print_token(Token *token) {
-    printf("%s\n", keyword_to_string(token->Value.keyword));
+    fprintf(stdout, "Token ID: %s\n", Token_ID_to_string(token->ID));
+    if(token->ID == TOKEN_ID_KEYWORD)
+        fprintf(stdout, "Keyword: %s\n", keyword_to_string(token->Value.keyword));
 }
 
 int main() {
