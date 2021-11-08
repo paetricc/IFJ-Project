8d00ecdc8bff079c2d204ad97055e9dd3468e4ce
Šimon Vacek <simonvacky@email.cz>
2021-11-08 22:24:51 +0100

    feat: vytvoreni kostry pro testy lexikalniho analyzatoru
    Vcetne makefilu, do ktereho pridam jako dalsi cil run.
    Ten bude fungovat podobne jako v 1. projektu IALu, tzn. provede diff outputu
    programu s ocekavanym outputem programu.

---
+++ unit-tests/scanner-test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../scanner.h"
+
+int main() {
+    printf ("Testy na lexikální analyzátor - scanner.c\n");
+    printf ("-----------------------------------------\n");  
+    return 0;
+}
