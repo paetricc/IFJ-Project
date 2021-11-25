5f3e361861001a82f68f51fc80f44da95ab92301
vitjanecek <vita.jp2@seznam.cz>
2021-11-25 17:15:18 +0100

    prvotni implementace TS, zacatek testovani

--- symtable.c
+++ symtable.c
@@ -9,26 +9,26 @@ void SLL_Param_Init(SLList_Param *listParam){
     listParam->lastElement = NULL;
 }
 
-void SLL_Param_Insert(SLList_Param *listParam, Data_type_local type, Dynamic_string *stringName){
+void SLL_Param_Insert(SLList_Param *listParam, Data_type_local type, Dynamic_string *stringName, bst_node_t *tree){
     SLLElementPtr_Param nElemPtr = (SLLElementPtr_Param)malloc(sizeof(struct SLLElement_Param));  //naalokuje prostor pro pridavany prvek
     if (nElemPtr == NULL){
         return;
     }
-    if (listParam->lastElement == NULL) {
+    if (tree->funcData->paramList->lastElement == NULL) {
         nElemPtr->type = type;
         if (!(DS_Copy(stringName, nElemPtr->name))){
             free(stringName);
         }
         nElemPtr->previousElement = NULL;
-        listParam->lastElement = nElemPtr;
+        tree->funcData->paramList->lastElement = nElemPtr;
     }
     if (listParam->lastElement != NULL) {
         nElemPtr->type = type;
         if (!(DS_Copy(stringName, nElemPtr->name))){
             free(stringName);
         }
-        nElemPtr->previousElement = listParam->lastElement;          //prenastavi ukazatele
-        listParam->lastElement = nElemPtr;
+        nElemPtr->previousElement = tree->funcData->paramList->lastElement;          //prenastavi ukazatele
+        tree->funcData->paramList->lastElement = nElemPtr;
     }
 }
 
@@ -74,55 +74,53 @@ void SLL_Return_Dispose(SLList_Return *listReturn){
     listReturn->lastElement = NULL;
 }
 
-void setDataV(Table_data_var_t *dataVar, Data_type_local type, bool init, bool used){
-    if (dataVar == NULL){
-        dataVar = malloc(sizeof(Table_data_var_t));
-    }
-    dataVar->type = type;
-    dataVar->init = init;
-    dataVar->used = used;
+void setDataV(bst_node_t *tree, Data_type_local type, bool init, bool used){
+    tree->varData->type = type;
+    tree->varData->init = init;
+    tree->varData->used = used;
 }
 
-void setDataF(Table_data_func_t *dataFunc, SLList_Param *listParam, SLList_Return *listReturn, bool def){
-    if (dataFunc == NULL){
-        dataFunc = malloc(sizeof(Table_data_func_t));
-    }
-    dataFunc->paramList = listParam;
-    dataFunc->returnList = listReturn;
-    dataFunc->def = def;
+void setDataF(bst_node_t *tree, SLList_Param *listParam, SLList_Return *listReturn, bool def){
+    SLL_Param_Init(listParam);
+    tree->funcData->paramList = listParam;
+    SLL_Return_Init(listReturn);
+    tree->funcData->returnList = listReturn;
+    tree->funcData->def = def;
 }
 
 void bst_init(bst_node_t **tree) {
     *tree = NULL;
 }
 
-bool bst_search(bst_node_t *tree, Dynamic_string *string) {
+bool bst_search(bst_node_t *tree, bst_node_t *findNode, Dynamic_string *string) {
     if (tree != NULL){                                               //dokud nedosel nakonec
-        if ((strcmp(tree->name->str, string->str)) == 0){                         //pokud jsou klice stejne, nahraje hodnotu a vrati true
+        if ((strcmp(tree->name->str, string->str)) == 0){//pokud jsou klice stejne, nahraje hodnotu a vrati true
+            findNode = tree;
             return true;
         } else{
             if ((strcmp(tree->name->str, string->str)) > 0){
-                return bst_search(tree->left, string);          //pokud je klic mensi, rekurzivne se zavola pro levy podstrom
+                return bst_search(tree->left, findNode,string);          //pokud je klic mensi, rekurzivne se zavola pro levy podstrom
             } else{
-                return bst_search(tree->right, string);         //pokud je klic vetsi, rekurzivne se zavola pro pravy podstrom
+                return bst_search(tree->right, findNode,string);         //pokud je klic vetsi, rekurzivne se zavola pro pravy podstrom
             }
         }
     } else{
+        findNode = NULL;
         return false;                                               // jestli nenasel
     }
 }
 
-void bst_insert(bst_node_t **tree, Dynamic_string *string, Table_data_func_t *dataFunc, Table_data_var_t *dataVar, bool isFnc) {
+void bst_insert(bst_node_t **tree, Dynamic_string *string, bool isFnc) {
     if (*tree != NULL){                                         //dokud nedosel nakonec
         if ((strcmp((*tree)->name->str, string->str)) == 0){//pokud jsou klice stejne, nahraje hodnotu a vrati true
             //error
             return;
         } else{
             if ((strcmp((*tree)->name->str, string->str)) > 0){
-                bst_insert(&((*tree)->left), string, dataFunc, dataVar, isFnc);       //pokud je klic mensi, rekurzivne se zavola pro levy podstrom
+                bst_insert(&((*tree)->left), string, isFnc);       //pokud je klic mensi, rekurzivne se zavola pro levy podstrom
                 return;
             } else{
-                bst_insert(&((*tree)->right), string, dataFunc, dataVar, isFnc);      //pokud je klic vetsi, rekurzivne se zavola pro pravy podstrom
+                bst_insert(&((*tree)->right), string, isFnc);      //pokud je klic vetsi, rekurzivne se zavola pro pravy podstrom
                 return;
             }
         }
@@ -134,9 +132,11 @@ void bst_insert(bst_node_t **tree, Dynamic_string *string, Table_data_func_t *da
             (*tree)->name->str = string->str;//inicializuje uzel
             (*tree)->isFnc = isFnc;
             if (isFnc){
-                (*tree)->funcData = dataFunc;
+                (*tree)->funcData = malloc(sizeof(Table_data_func_t));
+                (*tree)->varData = NULL;
             } else{
-                (*tree)->varData = dataVar;
+                (*tree)->varData = malloc(sizeof(Table_data_var_t));
+                (*tree)->funcData = NULL;
             }
             (*tree)->left = NULL;
             (*tree)->right = NULL;
@@ -166,18 +166,17 @@ void SLL_Frame_Insert(SLList_Frame *listFrame, bst_node_t *tree){
     if (nElemPtr == NULL){
         return;
     }
+    bst_init(&tree);
+    nElemPtr->node = tree;         //nahraje data
     if (listFrame->globalElement == NULL){
-        nElemPtr->node = tree;         //nahraje data
         nElemPtr->previousElement = NULL;
         listFrame->globalElement = nElemPtr;
     }
     if (listFrame->lastElement == NULL) {
-        nElemPtr->node = tree;         //nahraje data
         nElemPtr->previousElement = listFrame->globalElement;
         listFrame->lastElement = nElemPtr;
     }
     if (listFrame->lastElement != NULL) {
-        nElemPtr->node = tree;         //nahraje data
         nElemPtr->previousElement = listFrame->lastElement;          //prenastavi ukazatele
         listFrame->lastElement = nElemPtr;
     }
@@ -224,32 +223,49 @@ void SLL_Frame_Dispose(SLList_Frame *listFrame){
     listFrame->lastElement = NULL;
 }
 
-/**bst_node_t *search_Iden(Dynamic_string *string, SLList_Frame *listFrame){
-    SLLElementPtr_Frame elemPtr;
-    elemPtr = listFrame->lastElement;
-    SLLElementPtr_Frame pElemPtr;
+bst_node_t *search_Iden(Dynamic_string *string, SLList_Frame *listFrame){
+    SLList_Frame *elemPtr;
+    elemPtr = listFrame;
+    SLList_Frame *pElemPtr;
+    bst_node_t *treePtr;
     if (listFrame->lastElement != NULL){
         while (elemPtr->lastElement->previousElement != elemPtr->globalElement){
-            pElemPtr = elemPtr->previousElement;
-            if(bst_search(elemPtr->node, string)){
-                return elemPtr->node;
+            pElemPtr->lastElement = elemPtr->lastElement->previousElement;
+            if(bst_search(elemPtr->lastElement->node, treePtr,string)){
+                return treePtr;
             } else{
                 elemPtr = pElemPtr;
             }
         }
         if (elemPtr->lastElement->previousElement == elemPtr->globalElement){
-            if(DS_Copy(string, elemPtr->node->name->str)){
-                return elemPtr->node;
+            if(bst_search(elemPtr->lastElement->node, treePtr, string)){
+                return treePtr;
             }
         }
     }
     if(listFrame->globalElement != NULL){
-        elemPtr = listFrame->globalElement;
-        return elemPtr->node;
+        elemPtr = listFrame;
+        if(bst_search(elemPtr->globalElement->node, treePtr,string)){
+            return treePtr;
+        }
     }
-    return elemPtr->node = NULL;
-}**/
+    treePtr = NULL;
+    return treePtr;
+}
 
-//Table_data_func_t *getDataF(bst_node_t *, SLLElementPtr_Frame *);
+Table_data_func_t *getDataF(bst_node_t *tree){
+    /**Table_data_func_t *elemPtr;
+    if (tree == NULL){
+        return elemPtr = NULL;
+    } else{
+        elemPtr->def = tree->funcData->def;
+        elemPtr->paramList = tree->funcData->paramList;
+        elemPtr->returnList = tree->funcData->returnList;
+        return elemPtr;
+    }**/
+    return tree->funcData;
+}
 
-//Table_data_var_t *getDataV(bst_node_t *, SLLElementPtr_Frame *);
+Table_data_var_t *getDataV(bst_node_t *tree){
+    return tree->varData;
+}
