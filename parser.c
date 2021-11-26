9bf422928679ac9fdb0b60e83edf511c939b950a
Šimon Vacek <simonvacky@email.cz>
2021-11-26 23:18:44 +0100

    fix: pridana funkce na ignorovani bilych znaku tokenu
    Navic fix podminky "ifj21" a oprava preklepu v dokumentaci

--- parser.c
+++ parser.c
@@ -5,16 +5,38 @@
 #include <string.h>
 
 /**
+ * @brief Nacita tokeny dokud je v nich nektery bily znak, nebo az se narazi na EOF.
+ *
+ * @param token Token, ktery bude naplnen scannerem
+ * @param sourceFile Zdrojovy soubor cteny scannerem
+ *
+ * @return Typ erroru predany scannerem
+*/
+int get_non_white_token(Token *token, FILE *sourceFile) {
+  int error;
+  do {
+    // nacteni noveho tokenu a kontrola vysupu scanneru
+    if((error = get_token(token, sourceFile)) != ERROR_PASSED)
+	  // lexikalni nebo kompilatorova chyba
+      return error;
+  } while(token->ID != TOKEN_ID_EOF && ( 
+              token->ID == TOKEN_ID_SPACE || token->ID == TOKEN_ID_EOL ||
+              token->ID == TOKEN_ID_TAB));
+  return error;
+}
+
+/**
  * Pravidlo 1
  *
- * @param token Token, ktery bude naplni scanner
+ * @param token Token, ktery bude naplnen scannerem
  * @param sourceFile Zdrojovy soubor cteny scannerem
  * @return Typ erroru predany scannerem
 */
 int start(Token *token, FILE *sourceFile) {
-	int error = get_token(token, sourceFile);
-	if(error != ERROR_PASSED)	// lexikalni nebo kompilatorova chyba
-		return error;
+  int error;
+  if((error = get_non_white_token(token, sourceFile)))
+	  // lexikalni nebo kompilatorova chyba
+    return error;
 
 	// require
 	if(token->ID != TOKEN_ID_KEYWORD)
@@ -22,16 +44,17 @@ int start(Token *token, FILE *sourceFile) {
   else if(token->Value.keyword != KEYWORD_REQUIRE)
 		return ERROR_SYNTAX;
 
-	error = get_token(token, sourceFile);
-	if(error != ERROR_PASSED)	// lexikalni nebo kompilatorova chyba
-		return error;
+  if((error = get_non_white_token(token, sourceFile)))
+	  // lexikalni nebo kompilatorova chyba
+    return error;
 
 	// "ifj21"
 	if(token->ID != TOKEN_ID_FSTR)
 		return ERROR_SYNTAX;
-	else if(!strcmp(token->Value.string->str, "ifj21"))
+	else if(strcmp(token->Value.string->str, "\"ifj21\""))
 		return ERROR_SYNTAX;
 
+  // vse korektni - uplatnuju pravidlo a rozsiruju dalsi neterminal
   return program(token, sourceFile);
 }
 
