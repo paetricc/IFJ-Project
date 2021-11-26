65438f389ff1212cc891a179d4bad9693ffe9e28
Šimon Vacek <simonvacky@email.cz>
2021-11-27 00:47:19 +0100

    :sparkles: neterminal program
    Pro jeho implementaci je treba uz tabulky symbolu (konkretne pro rozhodnuti,
    zda pouzit pravidlo 3 - treba rozeznat id promenne od id funkce).

--- parser.c
+++ parser.c
@@ -10,7 +10,7 @@
  * @param token Token, ktery bude naplnen scannerem
  * @param sourceFile Zdrojovy soubor cteny scannerem
  *
- * @return Typ erroru predany scannerem
+ * @return Typ erroru generovany analyzou
 */
 int get_non_white_token(Token *token, FILE *sourceFile) {
   int error;
@@ -26,11 +26,13 @@ int get_non_white_token(Token *token, FILE *sourceFile) {
 }
 
 /**
- * Pravidlo 1
+ * @brief Neterminal start.
+ *
+ * Implementuje pravidlo 1.
  *
  * @param token Token, ktery bude naplnen scannerem
  * @param sourceFile Zdrojovy soubor cteny scannerem
- * @return Typ erroru predany scannerem
+ * @return Typ erroru generovany analyzou
 */
 int start(Token *token, FILE *sourceFile) {
   int error;
@@ -55,20 +57,73 @@ int start(Token *token, FILE *sourceFile) {
 		return ERROR_SYNTAX;
 
   // vse korektni - uplatnuju pravidlo a rozsiruju dalsi neterminal
-  return program(token, sourceFile);
+  return program(token, sourceFile); // aplikace pravidla 1
 }
 
 /**
- * Pravidlo 2
+ * @brief Neterminal program.
+ *
+ * Implementuje pravidla 2, 3, 4, 5.
  *
  * @param token Token, ktery bude naplni scanner
  * @param sourceFile Zdrojovy soubor cteny scannerem
- * @return Typ erroru predany scannerem
+ * @return Typ erroru generovany analyzou
 */
 int program(Token *token, FILE *sourceFile) {
-  return 0;
-}
+  int error;
+  if((error = get_non_white_token(token, sourceFile)))
+	  // lexikalni nebo kompilatorova chyba
+    return error;
+  
+  // rozvetveni na ruzna pravidla podle hodnoty tokenu
+  switch(token->ID) {
+    case TOKEN_ID_KEYWORD: // global nebo function
+      if(token->Value.keyword == KEYWORD_GLOBAL) { // global
+        error = fnc_dec(token, sourceFile); // aplikace pravidla 2
+        if(error)
+           return error;
+      }
+      else if(token->Value.keyword == KEYWORD_FUNCTION) { // function
+        error = fnc_def(token, sourceFile); // aplikace pravidla 4
+        if(error)
+          return error;
+      }
+      else { // prijate keyword nelze pouzit - pravidlo neexistuje
+        return ERROR_SYNTAX;
+      }
+    break;
 
+    case TOKEN_ID_ID: // id funkce
+      //TODO potrebuju symtable
+      Node *idFce = search(token->Value.string->str);
+      if (idFce == NULL) // id takove funkce neexistuje
+        return ERROR_SYNTAX;
+      if(!isVar(idFce) && isDeclared(idFce)) { // id funkce
+        error = fnc_call(token, sourceFile); // aplikace pravidla 3
+        if(error)
+          return error;
+      }
+      else // id promenne neni dovoleno => error
+        return ERROR_SYNTAX;
+    break;
+
+    case TOKEN_ID_EOF: // konec souboru - syntaxe je korektni, muze se ukoncit
+      return ERROR_PASSED;
+
+    default: // pro token zadne pravidlo neexistuje
+      return ERROR_SYNTAX;
+  } // switch
+
+  // rekurzivni volani neterminalu program
+  return program(token, sourceFile);
+} //program
+
+/**
+ * @brief Parser
+ *
+ * @param sourceFile Zdrojovy soubor cteny scannerem
+ * @return Typ erroru generovany analyzou
+*/
 int parser(FILE *sourceFile) {
 	// vytvoreni struktury token, kterou bude scanner plnit
 	Token *token = (Token *) malloc(sizeof(Token));
