739b2b0c53e89be1a6088f1fb309661c25c1af70
Šimon Vacek <simonvacky@email.cz>
2021-11-27 18:10:41 +0100

    :sparkles: Neterminal params_dec
    params_dec implementuje pravidla 7 a 8

--- parser.c
+++ parser.c
@@ -25,6 +25,7 @@ int get_non_white_token(Token *token, FILE *sourceFile) {
   return error;
 }
 
+
 /**
  * @brief Neterminal start.
  *
@@ -92,22 +93,22 @@ int program(Token *token, FILE *sourceFile) {
       }
     break;
 
-    case TOKEN_ID_ID: // id funkce
+    case TOKEN_ID_ID: // id_fnc
       //TODO potrebuju symtable
       Node *idFce = search(token->Value.string->str);
       if (idFce == NULL) // id takove funkce neexistuje
         return ERROR_SYNTAX;
-      if(!isVar(idFce) && isDeclared(idFce)) { // id funkce
+      if(!isVar(idFce) && isDeclared(idFce)) { // id_fnce
         error = fnc_call(token, sourceFile); // aplikace pravidla 3
         if(error)
           return error;
       }
-      else // id promenne neni dovoleno => error
+      else // id_var neni dovoleno => error
         return ERROR_SYNTAX;
     break;
 
     case TOKEN_ID_EOF: // konec souboru - syntaxe je korektni, muze se ukoncit
-      return ERROR_PASSED;
+      return ERROR_PASSED; // aplikace pravidla 5
 
     default: // pro token zadne pravidlo neexistuje
       return ERROR_SYNTAX;
@@ -129,6 +130,7 @@ int program(Token *token, FILE *sourceFile) {
 */
 int fnc_dec(Token *token, FILE *sourceFile) {
   // token global byl prijaty o uroven vyse => pokracuju dale
+  // aplikuju pravidlo 6
   int error;
 
   // id_fnc
@@ -142,7 +144,7 @@ int fnc_dec(Token *token, FILE *sourceFile) {
   symTable_Add_Fnc(token->Value.string->str);
 
   
-  // :
+  // ':'
   if((error = get_non_white_token(token, sourceFile)))
 	  // lexikalni nebo kompilatorova chyba
     return error;
@@ -162,7 +164,7 @@ int fnc_dec(Token *token, FILE *sourceFile) {
     return ERROR_SYNTAX;
 
   
-  // (
+  // '('
   if((error = get_non_white_token(token, sourceFile)))
 	  // lexikalni nebo kompilatorova chyba
     return error;
@@ -176,7 +178,7 @@ int fnc_dec(Token *token, FILE *sourceFile) {
     return error;
 
 
-  // )
+  // ')'
   if((error = get_non_white_token(token, sourceFile)))
 	  // lexikalni nebo kompilatorova chyba
     return error;
@@ -196,17 +198,62 @@ int fnc_dec(Token *token, FILE *sourceFile) {
 /**
  * @brief Neterminal params_dec.
  *
- * Implementuje pravidla 8,9.
+ * Implementuje pravidla 7,8.
  *
  * @param token Token, ktery bude naplni scanner
  * @param sourceFile Zdrojovy soubor cteny scannerem
  * @return Typ erroru generovany analyzou
 */
 int params_dec(Token *token, FILE *sourceFile) {
+  int error;
+
+  // promenne pro pripadne vraceni cteni pred zavorkovy token
+  fpos_t lastReadPos;
+  fgetpos(sourceFile, &lastReadPos);
+  if((error = get_non_white_token(token, sourceFile)))
+	  // lexikalni nebo kompilatorova chyba
+    return error;
+
+  // rozvetveni na ruzna pravidla podle hodnoty tokenu
+  switch(token->ID) {
+    case TOKEN_ID_RBR: // )
+      // vratim cteni pred zavorku, aby ho mohl precist volajici
+      fsetpos(sourceFile, &lastReadPos);
+      return ERROR_PASSED; // aplikace pravidla 7
+    break;
+
+    case TOKEN_ID_KEYWORD: // integer, number nebo string
+      if(token->Value.keyword == KEYWORD_INTEGER || token->Value.keyword == KEYWORD_NUMBER ||
+              token->Value.keyword == KEYWORD_STRING) {
+
+        // rozvinuti neterminalu data_type
+        if((error = data_type(token, sourceFile))) // aplikace pravidla 8
+          return error;
+      }
+      else // prijate keyword nelze pouzit - pravidlo neexistuje
+        return ERROR_SYNTAX;
+    break;
+
+    default: // pro token zadne pravidlo neexistuje
+      return ERROR_SYNTAX;
+  }
+
+  return params_dec2(token, sourceFile);
+}
+
+/**
+ * @brief Neterminal params_dec2.
+ *
+ * Implementuje pravidla 9, 10.
+ *
+ * @param token Token, ktery bude naplni scanner
+ * @param sourceFile Zdrojovy soubor cteny scannerem
+ * @return Typ erroru generovany analyzou
+*/
+int params_dec2(Token *token, FILE *sourceFile) {
   // TODO
   return ERROR_PASSED;
 }
-
 
 /**
  * @brief Neterminal return_type.
