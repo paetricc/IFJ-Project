1ee1c95bdc49787041bbb818c94478b948fe89ce
paetrik <tomasbartu00@gmail.com>
2021-11-30 12:31:49 +0100

    pridani deklarace funkci

--- expression.h
+++ expression.h
@@ -1,6 +1,13 @@
 #include "term_stack.h"
 #include "error.h"
+#include "./scanner-test.h"
 
 TermsAndNonTerms convertTokenType_To_TermsAndNonTerms( Token_ID );
 
 int exprSyntaxCheck( Token *, FILE * );
+
+void skipNonPrintChar(Token *, FILE * );
+
+int checkRulesAndApply( TermStack * );
+
+int SA_isOK( TermStack * );
