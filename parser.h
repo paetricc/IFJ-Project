3d99f0ee952ec88075e5c158d4dd385934cacd1d
Šimon Vacek <simonvacky@email.cz>
2021-12-03 00:28:57 +0100

    :sparkles: Priprava funkci na semantickou analyzu

--- parser.h
+++ parser.h
@@ -1,23 +1,25 @@
 #ifndef _PARSER_H_
 #define _PARSER_H_
 
+#include "symtable.h"
+
 int parser(FILE *);
 
 /*  funkce odpovidajici neterminalum v pravidlech */
 int start(Token *, FILE *);
 int program(Token *, FILE *);
 int fnc_dec(Token *, FILE *);
-int params_dec(Token *, FILE *);
-int params_dec2(Token *, FILE *);
-int return_type(Token *, FILE *);
-int data_type(Token *, FILE *);
+int params_dec(Token *, FILE *, bst_node_t *);
+int params_dec2(Token *, FILE *, bst_node_t *);
+int return_type(Token *, FILE *, bst_node_t *);
+int data_type(Token *, FILE *, bst_node_t *, bool);
 int fnc_call(Token *, FILE *);
-int value(Token *, FILE *);
-int value2(Token *, FILE *);
-int value_last(Token *, FILE *);
+int value(Token *, FILE *, bst_node_t *);
+int value2(Token *, FILE *, bst_node_t *);
+int value_last(Token *, FILE *, bst_node_t *);
 int fnc_def(Token *, FILE *);
-int fnc_head(Token *, FILE *);
-int fnc_def2(Token *, FILE *);
+int fnc_head(Token *, FILE *, bst_node_t *);
+int fnc_def2(Token *, FILE *, bst_node_t *);
 int params_def(Token *, FILE *);
 int params_def2(Token *, FILE *);
 int var_def(Token *, FILE *);
